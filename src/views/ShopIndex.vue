ce729ca763b4b6081bdd76f29497c6add9e65147
Gia Huy <giahuytrieu3@gmail.com>
2023-10-26 11:34:04 +0700

    Removed product descriptionh (sub)

--- src/views/ShopIndex.vue
+++ src/views/ShopIndex.vue
@@ -7,7 +7,6 @@
       <div class="main">
         <!-- main navbar -->
         <div class="main-navbar">
-          
           <!-- search bar -->
           <div class="input-box">
             <input type="text" placeholder="Search..." />
@@ -41,16 +40,15 @@
           </div>
           <hr class="divider" />
           <!-- List of books -->
-          <div class="main-detail">
+          <div class="main-product">
             <h2 class="main-title">Chọn sách</h2>
-            <!-- Detail wrapper -->
-            <div class="detail-wrapper">
-              <div v-for="book in books" :key="book.id" class="detail-card">
-                <img class="detail-img" :src="book.imageUrl" />
-                <div class="detail-desc">
-                  <div class="detail-name">
+            <!-- product wrapper -->
+            <div class="product-wrapper">
+              <div v-for="book in books" :key="book.id" class="product-list">
+                <img class="product-img" :src="book.imageUrl" />
+                <div class="product-desc">
+                  <div class="product-name">
                     <h4>{{ book.name }}</h4>
-                    <p class="detail-sub">{{ book.description }}</p>
                     <p class="price">{{ book.price }}</p>
                   </div>
                 </div>
@@ -120,84 +118,72 @@ export default {
           id: 1,
           imageUrl: 'book1.png',
           name: 'Book 1',
-          description: 'Description of Book 1',
           price: '99.000đ'
         },
         {
           id: 2,
           imageUrl: 'book2.png',
           name: 'Book 2',
-          description: 'Description of Book 2',
           price: '99.000đ'
         },
         {
           id: 3,
           imageUrl: 'book3.png',
           name: 'Book 3',
-          description: 'Description of Book 3',
           price: '99.000đ'
         },
         {
           id: 4,
           imageUrl: 'book4.png',
           name: 'Book 4',
-          description: 'Description of Book 4',
           price: '99.000đ'
         },
         {
           id: 5,
           imageUrl: 'book5.png',
           name: 'Book 5',
-          description: 'Description of Book 5',
           price: '99.000đ'
         },
         {
           id: 6,
           imageUrl: 'book6.png',
           name: 'Book 6',
-          description: 'Description of Book 6',
           price: '99.000đ'
         },
         {
           id: 7,
           imageUrl: 'book7.png',
           name: 'Book 7',
-          description: 'Description of Book 7',
           price: '99.000đ'
         },
         {
           id: 8,
           imageUrl: 'book8.png',
           name: 'Book 8',
-          description: 'Description of Book 8',
           price: '99.000đ'
         },
         {
           id: 9,
           imageUrl: 'book9.png',
           name: 'Book 9',
-          description: 'Description of Book 9',
           price: '99.000đ'
         },
         {
           id: 10,
           imageUrl: 'book10.png',
           name: 'Book 10',
-          description: 'Description of Book 10',
           price: '99.000đ'
         },
         {
           id: 11,
           imageUrl: 'book11.png',
           name: 'Book 11',
-          description: 'Description of Book 11',
           price: '99.000đ'
         },
         {
           id: 12,
           imageUrl: 'book12.png',
           name: 'Book 12',
-          description: 'Description of Book 12',
           price: '99.000đ'
         }
       ],
@@ -267,7 +253,6 @@ html {
   min-height: 100vh;
   padding: 2%;
   background-color: var(--softGreenColor);
-  /* margin-left: 250px; */
 }
 
 .main-navbar {
@@ -275,8 +260,8 @@ html {
   justify-content: space-between;
   align-items: center;
 }
-/* Search box */
 
+/* Search box */
 .input-box {
   position: relative;
   height: 55px;
@@ -350,7 +335,6 @@ html {
   pointer-events: auto;
   transform: translateY(-50%) rotate(180deg);
 }
-
 
 .cart {
   display: flex;
@@ -467,11 +451,11 @@ html {
   border: none;
 }
 
-.main-detail {
+.main-product {
   margin: 1rem 0 1.5rem 0;
 }
 
-.detail-wrapper {
+.product-wrapper {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   width: 100%;
@@ -480,7 +464,7 @@ html {
   border-radius: 8px;
 }
 
-.detail-card {
+.product-list {
   min-height: auto;
   height: 100%;
   background-color: var(--whiteColor);
@@ -490,7 +474,7 @@ html {
   cursor: pointer;
 }
 
-.detail-img {
+.product-img {
   width: 100%;
   height: auto;
   min-height: 430px;
@@ -499,37 +483,30 @@ html {
   object-position: center;
   filter: brightness(0.8);
 }
-
-.detail-desc {
+.product-desc {
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
   padding: 3%;
   width: 100%;
 }
-.detail-sub {
-  font-size: 12px;
-  color: var(--darkGreyColor);
-  letter-spacing: 0.5;
-  margin: 1% 0;
-}
 .price {
   color: var(--primaryColor);
 }
 
-.detail-card:hover {
+.product-list:hover {
   box-shadow: rgba(149, 157, 165, 0.2) 0 px 8px 24px;
 }
 
-.detail-card:hover .detail-img {
+.product-list:hover .product-img {
   filter: opacity(1);
 }
 
-.detail-card:hover .detail-name h4 {
+.product-list:hover .product-name h4 {
   color: var(--primaryColor);
 }
 
-.detail-card:hover .price {
+.product-list:hover .price {
   letter-spacing: 1.5px;
 }
 body::-webkit-scrollbar {
